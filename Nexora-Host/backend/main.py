95814ac6746542b0b13729236e9fd5a6dbaf55c6
pulsathost-png <pulsathost@gmail.com>
2026-08-20 19:04:53 +0300

    Create main.py

---
+++ Nexora-Host/backend/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from database import init_db
+
+app = Flask(__name__)
+CORS(app)
+
+init_db()
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "name": "Nexora Host",
+        "version": "1.0",
+        "status": "online"
+    })
+
+
+@app.route("/api/status")
+def status():
+    return jsonify({
+        "servers": "ready",
+        "system": "working"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

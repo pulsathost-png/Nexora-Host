9b1bac8bb83c68c550b95bae10b681fd1f8b728b
pulsathost-png <pulsathost@gmail.com>
2026-08-20 19:23:48 +0300

    Update main.py

--- Nexora-Host/backend/main.py
+++ Nexora-Host/backend/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+
 from database import init_db
+from auth import register, login
+
 
 app = Flask(__name__)
 CORS(app)
+
 
 init_db()
 
@@ -20,9 +24,20 @@ def home():
 @app.route("/api/status")
 def status():
     return jsonify({
-        "servers": "ready",
         "system": "working"
     })
+
+
+# Регистрация
+@app.route("/api/register", methods=["POST"])
+def register_user():
+    return register()
+
+
+# Вход
+@app.route("/api/login", methods=["POST"])
+def login_user():
+    return login()
 
 
 if __name__ == "__main__":
